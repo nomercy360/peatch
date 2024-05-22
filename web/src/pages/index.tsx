ca394e7c0cec8e0974303ef74ea68112e70db5fe
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2024-05-22 22:56:59 +0700

    fix

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -81,7 +81,9 @@ export default function Index() {
 					</Match>
 				</Switch>
 			</Link>
-			<ShufflePopup />
+			<Show when={store.user.published_at}>
+				<ShufflePopup />
+			</Show>
 			<Link class="flex flex-col items-start justify-start py-4" href="/users">
 				<div class="flex w-full flex-row items-center justify-start">
 					<Suspense fallback={<ImagesLoader />}>
