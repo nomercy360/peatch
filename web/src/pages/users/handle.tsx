2e922afb003ec56965bfcd6a41b92a87fd4807cd
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2024-06-10 09:01:20 +0700

    fix not found profile page

--- web/src/pages/users/handle.tsx
+++ web/src/pages/users/handle.tsx
@@ -8,7 +8,12 @@ import {
 	Suspense,
 	Switch,
 } from 'solid-js'
-import { useNavigate, useParams, useSearchParams } from '@solidjs/router'
+import {
+	Navigate,
+	useNavigate,
+	useParams,
+	useSearchParams,
+} from '@solidjs/router'
 import {
 	CDN_URL,
 	fetchProfile,
@@ -210,171 +215,181 @@ export default function UserProfilePage() {
 
 	return (
 		<div>
-			<Suspense fallback={<Loader />}>
-				<Switch>
-					<Match when={published() && isCurrentUserProfile}>
-						<ActionDonePopup
-							action="Profile published"
-							description="Now you can find people, create and join collaborations. Have fun!"
-							callToAction="There are 12 people you might be interested to collaborate with"
-						/>
-					</Match>
-					<Match when={!query.isLoading}>
-						<div class="h-fit min-h-screen bg-secondary">
-							<Show when={isCurrentUserProfile && store.user.hidden_at}>
-								<button
-									onClick={showInfoPopup}
-									class="absolute left-6 top-4 flex size-8 items-center justify-start"
-								>
-									<span class="material-symbols-rounded text-white">
-										visibility_off
-									</span>
-								</button>
-							</Show>
-							<Switch>
-								<Match when={isCurrentUserProfile}>
-									<div class="absolute right-4 top-4 z-10 flex h-8 items-center justify-center rounded-lg bg-white px-4 text-sm font-semibold text-[#FF8C42]">
-										42 Peatches
-									</div>
-								</Match>
-								<Match
-									when={!isCurrentUserProfile && !query.data?.is_following}
-								>
-									<ActionButton
-										disabled={unfollowMutate.isPending}
-										text="Follow"
-										onClick={follow}
-									/>
-								</Match>
-								<Match when={!isCurrentUserProfile && query.data?.is_following}>
-									<ActionButton
-										disabled={followMutate.isPending}
-										text="Unfollow"
-										onClick={unfollow}
-									/>
-								</Match>
-							</Switch>
-							<div class="p-2">
-								<img
-									src={CDN_URL + '/' + query.data.avatar_url}
-									alt="avatar"
-									class="aspect-square size-full rounded-xl object-cover"
-								/>
-							</div>
-							<div class="px-4 py-2.5">
-								<div class="flex flex-row items-center justify-between pb-4">
-									<Link
-										class="flex h-8 flex-row items-center space-x-2 text-sm font-semibold"
-										href={`/users/${query.data.id}/followers`}
-										state={{ back: true }}
-									>
-										<span class="flex flex-row items-center text-main">
-											{query.data.following_count}
-										</span>
-										<span class="text-secondary">following</span>
-										<span class="text-secondary">·</span>
-										<span class="flex flex-row items-center text-main">
-											{query.data.followers_count}
-										</span>
-										<span class="text-secondary">followers</span>
-									</Link>
+			<Switch>
+				<Match when={query.isLoading}>
+					<Loader />
+				</Match>
+				<Match when={query.isError}>
+					<Navigate href={'/404'} />
+				</Match>
+				<Match when={query.isSuccess}>
+					<Switch>
+						<Match when={published() && isCurrentUserProfile}>
+							<ActionDonePopup
+								action="Profile published"
+								description="Now you can find people, create and join collaborations. Have fun!"
+								callToAction="There are 12 people you might be interested to collaborate with"
+							/>
+						</Match>
+						<Match when={!query.isLoading}>
+							<div class="h-fit min-h-screen bg-secondary">
+								<Show when={isCurrentUserProfile && store.user.hidden_at}>
 									<button
-										class="flex h-8 flex-row items-center space-x-2 bg-transparent px-2.5"
-										classList={{
-											'text-main': !contentCopied(),
-											'text-green': contentCopied(),
-										}}
-										onClick={copyToClipboard}
+										onClick={showInfoPopup}
+										class="absolute left-6 top-4 flex size-8 items-center justify-start"
 									>
-										<span class="text-sm font-semibold">Share profile</span>
-										<span class="material-symbols-rounded text-[14px]">
-											{contentCopied() ? 'check_circle' : 'content_copy'}
+										<span class="material-symbols-rounded text-white">
+											visibility_off
 										</span>
 									</button>
-								</div>
-								<p class="text-3xl text-pink">
-									{query.data.first_name} {query.data.last_name}:
-								</p>
-								<p class="text-3xl text-main">{query.data.title}</p>
-								<p class="mt-1 text-lg font-normal text-secondary">
-									{query.data.description}
-								</p>
-								<div class="mt-5 flex flex-row flex-wrap items-center justify-start gap-1">
-									<For
-										each={
-											badgesExpanded()
-												? query.data.badges
-												: query.data.badges.slice(0, 3)
-										}
-									>
-										{badge => (
-											<div
-												class="flex h-10 flex-row items-center justify-center gap-[5px] rounded-2xl border px-2.5"
-												style={{
-													'background-color': `#${badge.color}`,
-													'border-color': `#${badge.color}`,
-												}}
-											>
-												<span class="material-symbols-rounded text-white">
-													{String.fromCodePoint(parseInt(badge.icon!, 16))}
-												</span>
-												<p class="text-sm font-semibold text-white">
-													{badge.text}
-												</p>
-											</div>
-										)}
-									</For>
-								</div>
-								<Show when={query.data.badges.length > 3}>
-									<ExpandButton
-										expanded={badgesExpanded()}
-										setExpanded={setBadgesExpanded}
-									/>
 								</Show>
-								<p class="py-4 text-3xl font-extrabold text-main">
-									Available for
-								</p>
-								<div class="flex w-full flex-col items-center justify-start gap-1">
-									<For
-										each={
-											opportunitiesExpanded()
-												? query.data.opportunities
-												: query.data.opportunities.slice(0, 3)
-										}
+								<Switch>
+									<Match when={isCurrentUserProfile}>
+										<div class="absolute right-4 top-4 z-10 flex h-8 items-center justify-center rounded-lg bg-white px-4 text-sm font-semibold text-[#FF8C42]">
+											42 Peatches
+										</div>
+									</Match>
+									<Match
+										when={!isCurrentUserProfile && !query.data?.is_following}
 									>
-										{op => (
-											<div
-												class="flex h-[60px] w-full flex-row items-center justify-start gap-2.5 rounded-2xl border px-2.5"
-												style={{
-													'background-color': `#${op.color}`,
-												}}
-											>
-												<div class="flex size-10 shrink-0 items-center justify-center rounded-full bg-secondary">
-													<span class="material-symbols-rounded shrink-0 text-main">
-														{String.fromCodePoint(parseInt(op.icon!, 16))}
+										<ActionButton
+											disabled={unfollowMutate.isPending}
+											text="Follow"
+											onClick={follow}
+										/>
+									</Match>
+									<Match
+										when={!isCurrentUserProfile && query.data?.is_following}
+									>
+										<ActionButton
+											disabled={followMutate.isPending}
+											text="Unfollow"
+											onClick={unfollow}
+										/>
+									</Match>
+								</Switch>
+								<div class="p-2">
+									<img
+										src={CDN_URL + '/' + query.data.avatar_url}
+										alt="avatar"
+										class="aspect-square size-full rounded-xl object-cover"
+									/>
+								</div>
+								<div class="px-4 py-2.5">
+									<div class="flex flex-row items-center justify-between pb-4">
+										<Link
+											class="flex h-8 flex-row items-center space-x-2 text-sm font-semibold"
+											href={`/users/${query.data.id}/followers`}
+											state={{ back: true }}
+										>
+											<span class="flex flex-row items-center text-main">
+												{query.data.following_count}
+											</span>
+											<span class="text-secondary">following</span>
+											<span class="text-secondary">·</span>
+											<span class="flex flex-row items-center text-main">
+												{query.data.followers_count}
+											</span>
+											<span class="text-secondary">followers</span>
+										</Link>
+										<button
+											class="flex h-8 flex-row items-center space-x-2 bg-transparent px-2.5"
+											classList={{
+												'text-main': !contentCopied(),
+												'text-green': contentCopied(),
+											}}
+											onClick={copyToClipboard}
+										>
+											<span class="text-sm font-semibold">Share profile</span>
+											<span class="material-symbols-rounded text-[14px]">
+												{contentCopied() ? 'check_circle' : 'content_copy'}
+											</span>
+										</button>
+									</div>
+									<p class="text-3xl text-pink">
+										{query.data.first_name} {query.data.last_name}:
+									</p>
+									<p class="text-3xl text-main">{query.data.title}</p>
+									<p class="mt-1 text-lg font-normal text-secondary">
+										{query.data.description}
+									</p>
+									<div class="mt-5 flex flex-row flex-wrap items-center justify-start gap-1">
+										<For
+											each={
+												badgesExpanded()
+													? query.data.badges
+													: query.data.badges.slice(0, 3)
+											}
+										>
+											{badge => (
+												<div
+													class="flex h-10 flex-row items-center justify-center gap-[5px] rounded-2xl border px-2.5"
+													style={{
+														'background-color': `#${badge.color}`,
+														'border-color': `#${badge.color}`,
+													}}
+												>
+													<span class="material-symbols-rounded text-white">
+														{String.fromCodePoint(parseInt(badge.icon!, 16))}
 													</span>
-												</div>
-												<div class="text-start text-white">
-													<p class="text-sm font-semibold">{op.text}</p>
-													<p class="text-xs leading-tight text-white/60">
-														{op.description}
+													<p class="text-sm font-semibold text-white">
+														{badge.text}
 													</p>
 												</div>
-											</div>
-										)}
-									</For>
-									<Show when={query.data.opportunities.length > 3}>
+											)}
+										</For>
+									</div>
+									<Show when={query.data.badges.length > 3}>
 										<ExpandButton
-											expanded={opportunitiesExpanded()}
-											setExpanded={setOpportunitiesExpanded}
+											expanded={badgesExpanded()}
+											setExpanded={setBadgesExpanded}
 										/>
 									</Show>
+									<p class="py-4 text-3xl font-extrabold text-main">
+										Available for
+									</p>
+									<div class="flex w-full flex-col items-center justify-start gap-1">
+										<For
+											each={
+												opportunitiesExpanded()
+													? query.data.opportunities
+													: query.data.opportunities.slice(0, 3)
+											}
+										>
+											{op => (
+												<div
+													class="flex h-[60px] w-full flex-row items-center justify-start gap-2.5 rounded-2xl border px-2.5"
+													style={{
+														'background-color': `#${op.color}`,
+													}}
+												>
+													<div class="flex size-10 shrink-0 items-center justify-center rounded-full bg-secondary">
+														<span class="material-symbols-rounded shrink-0 text-main">
+															{String.fromCodePoint(parseInt(op.icon!, 16))}
+														</span>
+													</div>
+													<div class="text-start text-white">
+														<p class="text-sm font-semibold">{op.text}</p>
+														<p class="text-xs leading-tight text-white/60">
+															{op.description}
+														</p>
+													</div>
+												</div>
+											)}
+										</For>
+										<Show when={query.data.opportunities.length > 3}>
+											<ExpandButton
+												expanded={opportunitiesExpanded()}
+												setExpanded={setOpportunitiesExpanded}
+											/>
+										</Show>
+									</div>
 								</div>
 							</div>
-						</div>
-					</Match>
-				</Switch>
-			</Suspense>
+						</Match>
+					</Switch>
+				</Match>
+			</Switch>
 		</div>
 	)
 }
